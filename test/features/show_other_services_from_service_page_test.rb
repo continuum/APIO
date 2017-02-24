186c081aa68e1f84765d098c1a082ce848979bb6
Carlos Fiebig <carlos.fiebig@continuum.cl>
2017-02-24 12:39:01 -0300

    Change user for test show_other_services...

--- test/features/show_other_services_from_service_page_test.rb
+++ test/features/show_other_services_from_service_page_test.rb
@@ -7,17 +7,17 @@ class ShowOtherServicesFromServicePageTest < Capybara::Rails::TestCase
   after { Warden.test_reset! }
 
   before do
-    login_as users(:perico), scope: :user
+    login_as users(:pedro), scope: :user
     @service_v = Service.create!(
       name: "PetsServiceName",
       organization: organizations(:sii),
       spec_file: File.open(Rails.root / "test/files/sample-services/petsfull.yaml")
-    ).create_first_version(users(:perico))
+    ).create_first_version(users(:pedro))
     @another_service_v = Service.create!(
       name: "EchoService",
       organization: organizations(:sii),
       spec_file: File.open(Rails.root / "test/files/sample-services/echo.yaml")
-    ).create_first_version(users(:perico))
+    ).create_first_version(users(:pedro))
   end
 
   test "Switch to another service of the same organization" do
