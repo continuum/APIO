35e7fc055e81a390022e4951df0e33ae3ef5ad7b
Carlos Fiebig <carlos.fiebig@continuum.cl>
2017-03-01 17:10:33 -0300

    Update file upload text for services and new versions

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
-    redirect_to services_path unless current_user.nil?
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
