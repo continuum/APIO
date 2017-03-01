d67f08a2f1978b365afde8a30a46ef581c006d0f
Carlos Fiebig <carlos.fiebig@continuum.cl>
2017-03-01 15:30:33 -0300

    Update login process to avoid it for logged users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
+    redirect_to services_path unless current_user.nil?
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
     else
